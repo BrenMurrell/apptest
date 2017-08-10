f3432baf7707871c048b6a0243aa114e6bc85bf6
BrenMurrell <bren.murrell@nvinteractive.com>
2017-08-10 19:16:31 +1200

    got posts working

---
+++ src/app/posts.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { Headers, Http} from '@angular/http';
+
+import 'rxjs/add/operator/toPromise';
+
+import { Post } from './post';
+
+@Injectable()
+export class PostsService {
+  private postsUrl = "http://blog.moon.co.nz/wp-json/wp/v2/";
+  private headers = new Headers({'Content-Type': 'application/json'});
+
+
+  constructor(private http: Http) { }
+  getPosts(): Promise<Post[]> {
+    return this.http.get(this.postsUrl + 'posts')
+      .toPromise()
+      .then(response => {
+        return response.json()
+      })
+      .catch(this.handleError);
+  }
+  private handleError(error: any): Promise<any>{
+      console.error('An error occurred', error); 
+      return Promise.reject(error.message || error);
+  }
+}
